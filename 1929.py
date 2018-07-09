b8aa393b99e6a929aa3b5016f10fa36793b23055
Dong Jun <enter09865@gmail.com>
2018-07-10 01:41:34 +0900

    1929

--- 1929.py
+++ 1929.py
@@ -1,3 +1,12 @@
 #소수 구하기
 m, n = input().split()
 
+for i in range(int(m), int(n)+1):
+    if i % 2 != 0:
+        count = 1
+        for j in range(2, i+1):
+            if i % j == 0 and count <= 2:
+                count += 1
+
+        if count == 2:
+            print(i)
